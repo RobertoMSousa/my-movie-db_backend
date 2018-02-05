b1de6789a145c2345cd85c7251147ba2f1ed40c7
Roberto Sousa <beto.sousa22@gmail.com>
2018-02-05 21:21:16 +0000

    create the placeholder user and test the mongo connection

--- test/app.test.ts
+++ test/app.test.ts
@@ -1,9 +1,36 @@
 import * as request from "supertest";
 import * as app from "../src/app";
+import * as mongoose from "mongoose";
+import * as path from "path";
+import * as dotenv from "dotenv";
 
-describe.skip("GET /random-url", () => {
-	it("should return 404", (done) => {
-		request(app).get("/auth/reset")
-			.expect(404, done);
+import { default as User, UserModel, AuthToken } from "../src/models/User";
+
+dotenv.config({ path: ".env" });
+
+describe.only("test the server", () => {
+
+	beforeAll(() => {
+		console.log("before all"); // roberto
+		// create the mongo user placeholder
+		const user = new User({
+			email: "placeholder@mail.com",
+			password: "123"
+		});
+		user.save((err: Error, document) => {
+			console.log("document-->", document); // roberto
+			return;
+		});
+	});
+
+	it("should be able to connect to the DB", (done) => {
+		mongoose.connect(process.env.MONGODB_TEST_URL, (err: Error) => {
+			if (err) {
+				done(err);
+				return;
+			}
+			done();
+			return;
+		});
 	});
 });
