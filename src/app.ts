248866c659d181c3709c2f292bf01ffb1c49919e
Roberto Sousa <beto.sousa22@gmail.com>
2018-02-20 01:36:08 +0000

    no message

--- src/app.ts
+++ src/app.ts
@@ -30,7 +30,7 @@ app.use(cookieParser());
 
 
 app.use(cors({
-	origin: ["http://localhost:3000", "https://my-movie-db-roberto.herokuapp.com"],
+	origin: "http://localhost:3000",
 	credentials: true,
 	methods: "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS",
 }));
